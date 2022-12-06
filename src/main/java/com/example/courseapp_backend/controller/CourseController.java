66ede44879fe581399a84777de0aae4b898e1fd0
roshmaep <roshmaep1999@gmail.com>
2022-12-06 11:22:54 +0530

    add

--- src/main/java/com/example/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/example/courseapp_backend/controller/CourseController.java
@@ -1,14 +1,21 @@
 package com.example.courseapp_backend.controller;
 
+import com.example.courseapp_backend.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
-    @PostMapping("/add")
-    public String CourseAdd(){
-        return "course addede successfully";
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String CourseAdd(@RequestBody Course c){
+        System.out.println(c.getCoursetitle().toString());
+        System.out.println(c.getDate().toString());
+        System.out.println(c.getDescription().toString());
+        System.out.println(c.getDuration().toString());
+        System.out.println(c.getVenue().toString());
+        return "course added successfully";
     }
     @GetMapping("/view")
     public String CourseView(){
