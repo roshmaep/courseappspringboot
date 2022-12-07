ada1dba5476a8ae183f0cdb558d258c3de904904
roshmaep <roshmaep1999@gmail.com>
2022-12-07 11:54:59 +0530

    s

--- src/main/java/com/example/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/example/courseapp_backend/controller/CourseController.java
@@ -5,7 +5,9 @@ import com.example.courseapp_backend.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CourseController {
@@ -13,14 +15,16 @@ public class CourseController {
     private CourseDao dao;
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
-    public String CourseAdd(@RequestBody Course c){
+    public Map<String,String> CourseAdd(@RequestBody Course c){
         System.out.println(c.getCoursetitle().toString());
         System.out.println(c.getDate().toString());
         System.out.println(c.getDescription().toString());
         System.out.println(c.getDuration().toString());
         System.out.println(c.getVenue().toString());
         dao.save(c);
-        return "course added successfully";
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
     }
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
